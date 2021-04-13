1d05532ea6bc2a0fc687b044dac2477ebc367f82
19Makos99 <45605294+19Makos99@users.noreply.github.com>
2021-04-13 19:29:06 +0200

    Ideiglenes kód eltávolítása

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-/*const TargyModel = require("./models/tantargy");
-const DiakModel = require("./models/diak");
-
-let prog = new TargyModel();
-prog.nev = "Prog";
-prog.save((err) => {
-    if (err)
-        console.log(err);
-    else
-        console.log("Done");
-});
-
-let Greg = new DiakModel();
-Greg.nev = "Greg";
-Greg.lakcim = "Pilisvörösvár";
-Greg.azonosito = "123456789";
-Greg.targyak = [{
-    felveve: true,
-    nev: prog.nev,
-    targyid: prog
-}];
-Greg.save();
-/**/
 let express = require("express");
 let app = express();
 let bodyParser = require('body-parser');
@@ -29,8 +6,6 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded());
 
 require("./route.js")(app);
-
-//app.use(express.static("static"));
 
 const PORT = 3000;
 
